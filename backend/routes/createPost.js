7edaf1c59646ad307fc25500f6a4df6435c9e8dd
mansigohil2003@gmail.com <139317406+patharom@users.noreply.github.com>
2025-01-18 05:45:01 +0530

    update

--- backend/routes/createPost.js
+++ backend/routes/createPost.js
@@ -8,9 +8,28 @@ const POST = mongoose.model("POST")
 router.get("/allposts", requireLogin, (req, res) => {
     POST.find()
         .populate("postedBy", "_id name Photo")
+        .populate("comments.postedBy", "_id name")
         .then(posts => res.json(posts))
         .catch(err => console.log(err))
 })
+
+router.post("/createPost", requireLogin, (req, res) => {
+    const { body, pic } = req.body;
+    console.log(pic)
+    if (!body || !pic) {
+        return res.status(422).json({ error: "Please add all the fields" })
+    }
+    console.log(req.user)
+    const post = new POST({
+        body,
+        photo: pic,
+        postedBy: req.user
+    })
+    post.save().then((result) => {
+        return res.json({ post: result })
+    }).catch(err => console.log(err))
+})
+
 
 router.get("/myposts", requireLogin, (req, res) => {
     POST.find({ postedBy: req.user._id })
@@ -51,21 +70,47 @@ router.put("/unlike", requireLogin, (req, res) => {
         })
 })
 
-router.post("/createPost", requireLogin, (req, res) => {
-    const { body, pic } = req.body;
-    console.log(pic)
-    if (!body || !pic) {
-        return res.status(422).json({ error: "Please add all the fields" })
+router.put("/comment", requireLogin, (req, res) => {
+    const comment = {
+        comment: req.body.text,
+        postedBy: req.user._id
     }
-    console.log(req.user)
-    const post = new POST({
-        body,
-        photo: pic,
-        postedBy: req.user
+    POST.findByIdAndUpdate(req.body.postId, {
+        $push: { comments: comment }
+    }, {
+        new: true
     })
-    post.save().then((result) => {
-        return res.json({ post: result })
-    }).catch(err => console.log(err))
+        .populate("comments.postedBy", "_id name")
+        .populate("postedBy", "_id name Photo")
+        .exec((err, result) => {
+            if (err) {
+                return res.status(422).json({ error: err })
+            } else {
+                res.json(result)
+            }
+        })
+})   
+
+
+// Api to delete post
+router.delete("/deletePost/:postId", requireLogin, (req, res) => {
+    POST.findOne({ _id: req.params.postId })
+        .populate("postedBy", "_id")
+        .exec((err, post) => {
+            if (err || !post) {
+                return res.status(422).json({ error: err })
+            }
+
+            if (post.postedBy._id.toString() == req.user._id.toString()) {
+
+                post.remove()
+                    .then(result => {
+                        return res.json({ message: "Successfully deleted" })
+                    }).catch((err) => {
+                        console.log(err)
+                    })
+            }
+        })
 })
 
 
--- frontend/src/components/Profie.js
+++ frontend/src/components/Profie.js
@@ -1,91 +1,76 @@
 import React, { useEffect, useState } from "react";
-import "./Home.css";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import PostDetail from "./PostDetail";
+import "./Profile.css";
 
-export default function Home() {
-  // Default image if user does not have a profile photo
-  const picLink = "https://cdn-icons-png.flaticon.com/128/3177/3177440.png";
-  const navigate = useNavigate();
-  const [data, setData] = useState([]); // Posts data
-  const [loading, setLoading] = useState(true); // Loading state
 
-  // Fetch posts after component mounts
-  useEffect(() => {
-    const token = localStorage.getItem("jwt");
-    if (!token) {
-      navigate("/signup"); // Redirect to signup if no JWT token
-      return;
+export default function Profie() {
+  const [pic, setPic] = useState([]);
+  const [show, setShow] = useState(false)
+  const [posts, setPosts] = useState([]);
+
+  const toggleDetails = (posts) => {
+    if (show) {
+      setShow(false);
+    } else {
+      setShow(true);
+      setPosts(posts);
     }
-
-    // Fetch all posts from the backend
-    fetch("http://localhost:5000/allposts", {
+  };
+ 
+  useEffect(() => {
+    fetch(`http://localhost:5000/user/${JSON.parse(localStorage.getItem("user"))._id}`, {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("jwt"),
       },
     })
       .then((res) => res.json())
       .then((result) => {
-        console.log(result); // Log fetched posts
-        if (Array.isArray(result)) {
-          setData(result); // Set posts to state if the result is an array
-        } else {
-          setData([]); // Ensure data is an empty array if result is not valid
-        }
-        setLoading(false); // Set loading to false after data is fetched
-      })
-      .catch((err) => {
-        console.error(err);
-        setLoading(false); // Set loading to false in case of error
+        console.log(result)
       });
-  }, [navigate]);
+  }, []);
 
   return (
-    <div className="home">
-      {loading ? (
-        <div className="loading">
-          <p>Loading posts...</p> {/* Loading indicator */}
+    <div className="profile">
+      {/* Profile frame */}
+      <div className="profile-frame">
+        {/* profile-pic */}
+        <div className="profile-pic">
+          <img
+            alt=""
+          />
         </div>
-      ) : (
-        data.length > 0 ? (
-          data.map((posts) => (
-            <div className="card" key={posts._id}>
-              {/* Card Header */}
-              <div className="card-header">
-                <div className="card-pic">
-                  <img
-                    src={posts.postedBy?.Photo ? posts.postedBy.Photo : picLink}
-                    alt={posts.postedBy?.name || "User profile picture"}
-                  />
-                </div>
-                <h5>
-                  <Link to={`/profile/${posts.postedBy?._id}`}>
-                    {posts.postedBy?.name}
-                  </Link>
-                </h5>
-              </div>
-
-              {/* Card Image */}
-              <div className="card-image">
-                <img src={posts.photo} alt="Post" />
-              </div>
-
-              {/* Card Content */}
-              <div className="card-content">
-                <span className="material-symbols-outlined material-symbols-outlined-red">
-                  favorite
-                </span>
-                <p>{Array.isArray(posts.likes) ? posts.likes.length : 0} Likes</p>
-                <p>{posts.body}</p>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="no-posts">
-            <p>No posts available</p> {/* Message when no posts are found */}
+        {/* profile-data */}
+        <div className="pofile-data">
+          <h1>{JSON.parse(localStorage.getItem("user")).name}</h1>
+          <div className="profile-info" style={{ display: "flex" }}>
+            <p>posts</p>
+            <p> followers</p>
+            <p>following</p>
           </div>
-        )
-      )}
+        </div>
+      </div>
+      <hr
+        style={{
+          width: "90%",
+
+          opacity: "0.8",
+          margin: "25px auto",
+        }}
+      />
+      {/* Gallery */}
+      <div className="gallery">
+        {pic.map((pics) => {
+          return <img key={pics._id} src={pics.photo}
+            onClick={() => {
+              toggleDetails(pics)
+            }}
+            className="item"></img>;
+        })}
+      </div>
+      {show &&
+        <PostDetail item={posts} toggleDetails={toggleDetails} />
+      }
+      
     </div>
   );
 }
